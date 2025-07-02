dcac2fef6fcd0e67e7399c8b6326cc014fab14d8
Shiva <luxdrive0360@gmail.com>
2025-07-02 16:54:19 +0530

    driver service added

---
+++ src/test/java/com/luxdrive/DriverService/DriverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luxdrive.DriverService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DriverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
